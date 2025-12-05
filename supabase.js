adf8a6f2160e5241d8e0c1f534d3eeddba4187f0
grupodvbr <grupodvbr@gmail.com>
2025-12-05 16:24:18 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supa = createClient(
+  "https://ckkqcxbzfkjzicvoeehw.supabase.co",
+  "sb_publishable_djiQsCSrnLUm0iA_eNjjMw__7rfP11m"
+);
